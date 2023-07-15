d0adc63bfe58786a2dc2836deced801396448204
Suhaibusman <suhaibusman54@gmail.com>
2023-07-15 13:43:46 -0700

    Electronic Voting System

---
+++ EVS.dart
@@ -0,0 +1,15 @@
+void main(){
+
+print("==== ELECTRONIC VOTING SYSTEM ====");
+print("Select Candidates");
+print("Press 1 for PTI");
+print("Press 2 for PMLN");
+print("Press 3 for PPP");
+print("Press 4 for MQM");
+print("Press 5 for INDEPENDENT");
+
+
+
+}
+
+
